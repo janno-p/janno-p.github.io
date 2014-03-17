3a71959be50defa0079235240171de796115a8dc
Janno Põldma <janno.poldma@gmail.com>
2014-03-17 23:49:59 +0200

    Upgrade.

---
+++ idelo/js/idelo/register.js
@@ -0,0 +1,65 @@
+var users = (function () {
+  var users = null;
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': 'data/users.json',
+    'dataType': 'json',
+    'success': function (data) {
+      users = data;
+    }
+  });
+  return users;
+})();
+
+function setupValidation() {
+  $("form#login").validate({
+    showErrors: function (errorMap, errorList) {
+      $.each(this.validElements(), function (index, element) {
+        $(element).parent()
+                  .removeClass("has-error");
+        $(element).data("title", "")
+                  .tooltip("destroy");
+      });
+
+      $.each(errorList, function (index, error) {
+        $(error.element).parent()
+                        .addClass("has-error");
+        $(error.element).tooltip("destroy")
+                        .data("title", error.message)
+                        .tooltip({ placement: "bottom" });
+      });
+    },
+
+    submitHandler: function (form) {
+      var $email = $(form).find("input[type='email']");
+      var emailValue = $email.val();
+      if (emailValue == users[0].email) {
+        window.location.href = "index-citizen.htm";
+        return false;
+      }
+      if (emailValue == users[1].email) {
+        window.location.href = "index-official.htm";
+        return false;
+      }
+      $email.parent()
+            .addClass("has-error");
+      $email.tooltip("destroy")
+            .data("title", "Kasutaja sisselogimine ebaõnnestus!")
+            .tooltip({ placement: "bottom" });
+      $email.focus();
+      return false;
+    }
+  });
+}
+
+$(document).ready(function () {
+  $.get('partial/navbar.htm', function (data) {
+    $('body').prepend(data);
+    $.get('partial/navbar-login.htm', function (data) {
+      $('#navbar-container').append(data);
+      setupValidation();
+      $("#register-email").focus();
+    });
+  });
+});
---
+++ idelo/js/idelo/subjects-citizen.js
@@ -0,0 +1,87 @@
+var subjects = (function () {
+  var subjects = null;
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': 'data/citizens.json',
+    'dataType': 'json',
+    'success': function (data) {
+      subjects = data;
+    }
+  });
+  return subjects;
+})();
+
+$(document).ready(function () {
+  $.get('partial/navbar.htm', function (data) {
+    $('body').prepend(data);
+    $("a.navbar-brand").attr("href", "index-citizen.htm");
+    $.get('partial/navbar-citizen.htm', function (data) {
+      $('#navbar-container').append(data);
+    });
+  });
+
+  function fillSubjects () {
+    $("#subjects").find("tbody").empty();
+    $.each(subjects.slice(0,10), function (i, row) {
+      $("<tr>").append($("<td>").text(row.name))
+               .append($("<td>").text(row.birthDate))
+               .append($("<td>").text(row.gender))
+               .append($("<td>").text(row.address))
+               .appendTo($("#subjects").find("tbody"));
+    });
+  }
+
+  fillSubjects();
+
+  $("a.subject-sort").click(function () {
+    var $span = $(this).find("span");
+    var className = "glyphicon-sort-by-attributes";
+    var asc = true;
+    if ($span.hasClass(className)) {
+      className += "-alt";
+      asc = false;
+    }
+    $("a.subject-sort").find("span")
+                       .removeClass("glyphicon-sort-by-attributes")
+                       .removeClass("glyphicon-sort-by-attributes-alt")
+                       .addClass("glyphicon-sort");
+    $span.addClass(className);
+    switch ($(this).attr("data-id")) {
+      case "name":
+        subjects.sort(function (a, b) {
+          return (asc ? 1 : -1) * a.name.localeCompare(b.name);
+        });
+        break;
+      case "birthDate":
+        subjects.sort(function (a, b) {
+          return (asc ? 1 : -1) * a.birthDate.localeCompare(b.birthDate);
+        });
+        break;
+      case "gender":
+        subjects.sort(function (a, b) {
+          return (asc ? 1 : -1) * a.gender.localeCompare(b.gender);
+        });
+        break;
+      case "address":
+        subjects.sort(function (a, b) {
+          return (asc ? 1 : -1) * a.address.localeCompare(b.address);
+        });
+        break;
+    }
+    fillSubjects();
+  });
+
+  /*
+  var $tr =$('<tr>').addClass('header');
+        $.each(data.headers, function(i,header){
+            $tr.append($('<th>').append($('a').addClass('sort').attr('href','#').append($('span').text(header))));
+        });
+        $tr.appendTo('table.data');
+        $.each(data.rows,function(i,row){
+            $('<tr>').attr('id',i).
+                append($('<td>').text(row.date)).
+                append($('<td>').text(row.company)).
+                append($('<td>').text(row.location)).appendTo('table.data');
+        });*/
+});
